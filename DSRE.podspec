ad547d126e89973e5b1a4544f176255661ad5e61
anhdungaxz <dungpt.hust@gmail.com>
2024-04-05 11:13:23 +0700

    add alamofire

--- DSRE.podspec
+++ DSRE.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'DSRE'
-  s.version          = '0.1.2'
+  s.version          = '0.1.3'
   s.summary          = 'DSRE VNPT'
 
 # This description is used to generate tags and improve search results.
@@ -39,4 +39,5 @@ Pod::Spec.new do |s|
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
+  s.dependency 'Alamofire', '~> 5.0'
 end
---
+++ LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  DSRE
+//
+//  Created by Dũng Phạm Tiến  on 01/04/2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func login3g(_ sender: Any) {
+    }
+    
+    @IBAction func loginByPhone(_ sender: Any) {
+    }
+
+    @IBAction func actionDismiss(_ sender: Any) {
+        self.navigationController?.dismiss(animated: true)
+    }
+}
